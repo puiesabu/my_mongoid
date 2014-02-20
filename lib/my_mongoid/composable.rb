07954a4d572872990ec83cc9f1d500cf2c01327a
kaeli <puiesabu@gmail.com>
2014-02-20 15:24:31 +0800

    remove unused files

--- lib/my_mongoid/composable.rb
+++
@@ -1,7 +0,0 @@
-module MyMongoid
-
-  # This module provides inclusions of all behaviour in a Mongoid document.
-	module Composable
-  # TODO
- end
-end
--- lib/my_mongoid/duplicate_field_error.rb
+++
@@ -1,6 +0,0 @@
-module MyMongoid
-
-  # This error is raised when field is declared twice
-	class DuplicateFieldError < RuntimeError
-	end
-end
