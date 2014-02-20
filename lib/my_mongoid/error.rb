2ea1ecf23d325b3c54d5bfb3516994008c2bbfd4
kaeli <puiesabu@gmail.com>
2014-02-20 15:58:10 +0800

    update indentation

--- lib/my_mongoid/error.rb
+++ lib/my_mongoid/error.rb
@@ -1,10 +1,10 @@
 module MyMongoid
 
   # This error is raised when field is declared twice
-	class DuplicateFieldError < RuntimeError
-	end
+  class DuplicateFieldError < RuntimeError
+  end
 
   # This error is raised when field is undeclared
-	class UnknownAttributeError < RuntimeError
-	end
+  class UnknownAttributeError < RuntimeError
+  end
 end
